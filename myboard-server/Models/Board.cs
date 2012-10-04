eacf2034e411a30d2f236167913f2cfdbd4c46df
Jonathan Keith <jkeith@tradestation.com>
2012-10-04 10:36:26 -0500

    added models

---
+++ myboard-server/Models/Board.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myboard_server.Models
+{
+    public class Board
+    {
+        public List<Sticky> Stickies { get; set; }
+    }
+}
---
+++ myboard-server/Models/Sticky.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace myboard_server.Models
+{
+    public class Sticky
+    {
+        public string Id { get; set; }
+        public string Content { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
