ca6e931850f5558e710a84ec6cda8b055f37a306
joncse <jonkeith@gmail.com>
2012-10-24 07:28:40 -0500

    add ravendb and initializer

---
+++ myboard-server/Database/RavenDatabase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Raven.Client;
+using Raven.Client.Document;
+
+namespace myboard_server.Database
+{
+    public class RavenDatabase
+    {
+        private static IDocumentStore store;
+        private static object locker = new object();
+
+        public static RavenDatabase()
+        {
+            Initialize();
+        }
+
+        private static void Initialize()
+        {
+            if (store == null)
+            {
+                lock (locker)
+                {
+                    if (store == null)
+                    {
+                        store = new DocumentStore { ConnectionStringName = "RavenDB" };
+                        store.Conventions.IdentityPartsSeparator = "-";
+                        store.Initialize();
+                    }
+                }
+            }
+        }
+
+        public IDocumentSession GetSession()
+        {
+            return store.OpenSession();
+        }
+
+    }
+}
