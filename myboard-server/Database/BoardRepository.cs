412b1d969b1ab3f681faebba62fa7b257af3ace2
joncse <jonkeith@gmail.com>
2012-10-24 08:41:44 -0500

    added board repository

---
+++ myboard-server/Database/BoardRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using myboard_server.Models;
+using Raven.Client;
+
+namespace myboard_server.Database
+{
+    public class BoardRepository
+    {
+        private readonly IDocumentSession session;
+
+        public BoardRepository(IDocumentSession session)
+        {
+            this.session = session;
+        }
+
+        public Board Get()
+        {
+            Board board = default(Board);
+            board = this.session.Load<Board>(Board.BoardId);
+
+            if (board == null)
+            {
+                board = new Board();
+                this.session.Store(board);
+                this.session.SaveChanges();
+            }
+
+            return board;
+        }
+
+        public void Save(Board board)
+        {
+            this.session.Store(board);
+            this.session.SaveChanges();
+        }
+    }
+}
