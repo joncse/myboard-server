7f9e25d7b90502bf4ff5536268af73e306c1d827
Jonathan Keith <jkeith@tradestation.com>
2012-10-24 07:00:11 -0500

    added board controller

---
+++ myboard-server/Controllers/BoardController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using myboard_server.Models;
+
+namespace myboard_server.Controllers
+{
+    public class BoardController : ApiController
+    {
+        // GET api/board
+        public Board Get()
+        {
+            var board = new Board();
+            board.Stickies.Add(new Sticky { Content = "Hello World!", X = 5, Y = 10 });
+            board.Stickies.Add(new Sticky { Content = "Goodbye World!", X = 15, Y = 20 });
+
+            return board;
+        }
+    }
+}
--- myboard-server/Models/Board.cs
+++ myboard-server/Models/Board.cs
@@ -8,5 +8,10 @@ namespace myboard_server.Models
     public class Board
     {
         public List<Sticky> Stickies { get; set; }
+
+        public Board()
+        {
+            Stickies = new List<Sticky>();
+        }
     }
 }
